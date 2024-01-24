24bff844ec87d918888952a81bebd7d0253b067c
Engin <enginorhan2000@gmail.com>
2024-01-24 21:28:05 +0300

    register process added

--- appUser/views.py
+++ appUser/views.py
@@ -24,5 +24,31 @@ def loginPage(request):
     return render(request,"user/login.html",context)
 
 def registerPage(request):
+    
+    if request.method == "POST":
+        fname=request.POST.get("fname")
+        lname=request.POST.get("lname")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password1=request.POST.get("password1")
+        password2=request.POST.get("password2")
+    
+        if fname and lname and username and email and password1:
+            if password1==password2:
+                if not User.objects.filter(username=username).exists():
+                    if not User.objects.filter(email=email).exists():
+                        up_bool=False
+                        num_bol=False                 
+                        # Password Control
+                        for i in password1:
+                            if i.isnumeric():
+                                num_bol=True
+                            if i.isupper():
+                                up_bool=True
+                            if len(password1) >=8 and num_bol and up_bool:
+                            #Kayıt edilebilir
+                                user= User.objects.create_user(first_name=fname,last_name=lname,username=username,email=email,password=password1)
+                                user.save()
+                                return redirect('loginPage')
     context={}
     return render(request,"user/register.html",context)
